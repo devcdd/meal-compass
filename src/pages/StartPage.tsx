799a66999958bf5b6ec1e4e2c8e4b1d921d19a2e
디디 <developer.cdd@gmail.com>
2024-10-31 00:42:13 +0900

    feat: 룰렛 돌리는 로직 구현

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,7 +1,23 @@
-interface StartPageProps {}
+import Roulette from "../components/Roulette/Roulette.tsx";
+import Button from "../components/Button/Button.tsx";
+import { useRoulette } from "../hooks/useRoulette.ts";
+import VolumetricCanvas from "../components/Canvas/VolumetricCanvas.tsx";
 
-const StartPage = (props: StartPageProps) => {
-  return <></>;
+const StartPage = () => {
+  const { isClicked, handleSpin } = useRoulette();
+
+  const list = Array.from({ length: 10 }, (_, i) => ({
+    name: `Item ${i + 1}`,
+    id: i + 1,
+  }));
+
+  return (
+    <main>
+      <VolumetricCanvas />
+      <Roulette list={list} isClicked={isClicked} />
+      <Button onClick={handleSpin}>오늘의 메뉴를 랜덤하게 골라보세요 !</Button>
+    </main>
+  );
 };
 
 export default StartPage;
